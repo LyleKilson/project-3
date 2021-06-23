7c42f2d3ac6e3edef1a8dbbfb22a0150fbe623ca
Caroline Phillips <Caroline@Carolines-MacBook-Air.local>
2021-06-22 20:53:38 -0600

    react-setup

---
+++ client/src/components/CartItem/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { useStoreContext } from "../../utils/GlobalState";
+import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
+import { idbPromise } from "../../utils/helpers";
+
+
+export default CartItem;
+
